d2d139f9ed2ca8c6306604410b42a86e0d671d2c
anoopkarnik <danoopkarnik@gmail.com>
2025-01-29 00:14:16 +0530

    Add webhook payment correctly dodo

--- apps/nextjs-app/app/api/payments/dodo/webhook/route.ts
+++ apps/nextjs-app/app/api/payments/dodo/webhook/route.ts
@@ -1,37 +1,41 @@
 import crypto from 'crypto';
 import { NextResponse } from "next/server";
 import { HandleCheckoutSessionCompleted } from "../../../../_actions/payments/dodo";
+import { Webhook } from 'standardwebhooks';
+import { headers } from "next/headers"
 
+const webhook = new Webhook(process.env.DODO_WEBHOOK_SECRET as string);
 
 export async function POST(request: Request) {
-    const webhookId = request.headers.get('webhook-id');
-    const webhookTimestamp = request.headers.get('webhook-timestamp');
-    const webhookSignature = request.headers.get('webhook-signature');
-    const rawBody = await request.text();
-    const secret = process.env.DODO_WEBHOOK_SECRET;
+    const headersList = await headers();
 
-    const verificationString = `${webhookId}.${webhookTimestamp}.${rawBody}`;
+    try {
+        const rawBody = await request.text();
+    
+        const webhookHeaders = {
+          "webhook-id": headersList.get("webhook-id") || "",
+          "webhook-signature": headersList.get("webhook-signature") || "",
+          "webhook-timestamp": headersList.get("webhook-timestamp") || "",
+        };
+    
+        await webhook.verify(rawBody, webhookHeaders);
 
-    // Step 2: Compute the HMAC SHA256 signature
-    const computedSignature = crypto
-        .createHmac('sha256', secret as string)
-        .update(verificationString)
-        .digest('hex');
 
-    if (!safeCompare(computedSignature, webhookSignature)) {
-        console.error('Invalid webhook signature');
-        return new NextResponse('Invalid signature', { status: 400 });
+        const event = JSON.parse(rawBody);
+
+        HandleCheckoutSessionCompleted(event);
+
+        return new NextResponse(null, { status: 200 });
+    }catch (error) {
+        console.log("Webhook processing failed", error);
+        return Response.json(
+          {
+            error: "Webhook processing failed",
+            details: error instanceof Error ? error.message : "Unknown error",
+          },
+          { status: 400 }
+        );
     }
 
-    const event = JSON.parse(rawBody);
-
-    HandleCheckoutSessionCompleted(event);
-
-    return new NextResponse(null, { status: 200 });
 }
 
-function safeCompare(a:any, b:any) {
-    const len = Math.max(a.length, b.length);
-    return crypto.timingSafeEqual(Buffer.from(a.padEnd(len)), Buffer.from(b.padEnd(len)));
-  }
-  
